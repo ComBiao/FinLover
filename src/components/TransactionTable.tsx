2709167a948a53def45811a42d391777fee16722
Supanat Suwannarat <Supanat09020@gmail.com>
2026-09-11 18:35:47 +0700

    fix: correct TransactionTable mock import paths after rebase onto main

--- src/components/TransactionTable.tsx
+++ src/components/TransactionTable.tsx
@@ -29,8 +29,8 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { CATEGORY_STYLES, resolveChipTone, WALLET_TYPE_STYLES } from "@/components/chipColor";
-import { MOCK_CATEGORIES } from "@/components/mockCategories";
-import { MOCK_WALLETS } from "@/components/mockWallets";
+import { MOCK_CATEGORIES } from "@/mocks/mockCategories";
+import { MOCK_WALLETS } from "@/mocks/mockWallets";
 import { cn } from "@/lib/utils";
 import type { Transaction } from "@/types/transaction";
 
